f4b1125ff15650c755654ad4a79eb7a66ed11015
mdtanvirislamrakib <mdtanvirislamrakib7@gmail.com>
2025-04-14 14:41:04 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello React Router</div>,
+  },
+]);
+
+
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
